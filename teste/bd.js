163e363be6e03333299b1f3da4a2d2df278034e4
LucasGiovaniFRuck <lucas.f.giovani@gmail.com>
2025-03-06 22:06:13 -0300

    teste

---
+++ teste/bd.js
@@ -0,0 +1,56 @@
+const mysql = require('mysql2/promise');
+
+async function conectarBD()
+{
+
+    if (global.connection && global.connection.state !== 'disconnected')
+    {
+        return global.connection;
+    }
+
+    const conexao = await mysql.createConnection(
+        {
+            host: 'localhost',
+            port: '3306',
+            user: 'root',
+            password: '',
+            database: 'teste'
+        }
+    );
+
+    /* 
+    Outra forma de fazer a conexão:
+    const conexaoString = 'mysql://root:@localhost:3306/teste';
+    const conexao = await mysql.createConnection(conexaoString);
+    */
+
+    global.createConnection = conexao;
+
+    return global.connection;
+    
+}
+
+async function buscarUsuario(usuario)
+{
+    const conex = await conectarBD();
+    const sql = "select * from usuarios where usuemail=? and usussenha=?;";
+    const [usuarioEncontrado] = await conex.query(sql,[usuario.email, usuario.senha]);   
+
+    /* 
+    if (usuarioEncontrado && usuarioEncontrado.lenght>0)
+    {
+        return usuarioEncontrado[0];
+    }
+    else
+    {
+        return {};
+    }
+    Essa parte de cima é igual o de baixo*/
+
+    return usuarioEncontrado && usuarioEncontrado.lenght>0 ? usuarioEncontrado[0] : {}
+
+}
+
+conectarBD();
+
+module.exports = { buscarUsuario }
--- teste/routes/index.js
+++ teste/routes/index.js
@@ -3,7 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { titulo: 'Login', mensagem: 'Por favor identifique-se' });
+});
+
+router.post('/login', async function(req, res, next) {
+  const email = req.body.email;
+  const senha = req.body.senha;
+
+  // Consultar o BD e confirmar se o usuário existe.
+  //const usuario = await global.bd.buscarUsuario(email,senha);
+  //const usuario = await global.bd.buscarUsuario([email,senha]);
+  const usuario = await global.bd.buscarUsuario({email,senha});
+
+  if (usuario.usuemail && usuario.usuemail != "")
+  {
+    global.usuario = usuario.usuemail;
+    res.redirect('/perfis');
+  }
+  else
+  {
+    res.redirect('/');
+  }
+
+});
+
+router.get('/perfis', function(req, res, next ){
+  if (!global.usuario || global.usuario == "")
+  {
+    res.redirect('/');
+  }
+
+  res.render('prefil');
 });
 
 module.exports = router;
